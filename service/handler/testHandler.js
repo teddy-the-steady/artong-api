b7cfbc0e6819964bac9b4a3e5d54458f23e154ca
teddy <hohoworld11@gmail.com>
2020-03-04 20:49:54 +0900

    init.js 추가

--- service/handler/testHandler.js
+++ service/handler/testHandler.js
@@ -1,21 +1,8 @@
-const mysql = require('mysql2/promise');
 const httpRequest = require('../utils/http/request');
 const httpResponse = require('../utils/http/response');
 const testListController = require('../controllers/testListController');
 const testViewController = require('../controllers/testViewController');
 const testCreateController = require('../controllers/testCreateController');
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-});
-
-const ALLOWED_ORIGINS = [
-	'https://myfirstorigin.com',
-	'https://mysecondorigin.com'
-];
 
 module.exports.test = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
@@ -28,22 +15,22 @@ module.exports.test = async (event, context, callback) => {
     switch (requestInfo.httpMethod) {
       case 'GET':
         if (requestInfo.path === '/test/product' || requestInfo.path === '/test/product/')
-          res = await testListController.control(pool, requestInfo.queryStringParameters);
+          res = await testListController.control(requestInfo.queryStringParameters);
         else if (requestInfo.path.startsWith('/test/product/') && requestInfo.pathParameters)
-          res = await testViewController.control(pool, requestInfo.queryStringParameters, requestInfo.pathParameters);
+          res = await testViewController.control(requestInfo.queryStringParameters, requestInfo.pathParameters);
         break;
       case 'POST':
         requestInfo.body = JSON.parse(requestInfo.body);
         if (requestInfo.path === '/test/product' || requestInfo.path === '/test/product/')
-          res = await testCreateController.control(pool, requestInfo.body);
+          res = await testCreateController.control(requestInfo.body);
         break;
       default:
         break;
     }
   } catch (error) {
     console.log(error);
-    httpResponse.errorResponse(event, ALLOWED_ORIGINS, error, callback);
+    httpResponse.errorResponse(event, error, callback);
   }
 
-  httpResponse.successResponse(event, ALLOWED_ORIGINS, res, callback);
+  httpResponse.successResponse(event, res, callback);
 };
---
+++ service/init.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+module.exports.pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+module.exports.ALLOWED_ORIGINS = [
+  'https://myfirstorigin.com',
+  'https://mysecondorigin.com'
+];
