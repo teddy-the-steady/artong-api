b30604438f229cdfc738ddcddb1c539d492a1961
teddy <hohoworld11@gmail.com>
2020-10-11 17:35:55 +0900

    Revert "import 도입 디버그"
    This reverts commit 9bf5f0aed307a3815e930305e3716a768fa44332.

--- service/handler/test.ts
+++ service/handler/test.ts
@@ -1,8 +1,9 @@
-import init from '../init' // TODO] handler 이전 선언이 최초 이후 인스턴스 호출에도 유효한지 확인
-import {request} from '../utils/http/request'
-import {successResponse, errorResponse} from '../utils/http/response'
+export {};
+const init = require('../init');
+const httpRequest = require('../utils/http/request');
+const httpResponse = require('../utils/http/response');
 const testListController = require('../controllers/testListController');
-import {testViewController} from '../controllers/testViewController'
+const testViewController = require('../controllers/testViewController');
 const testCreateController = require('../controllers/testCreateController');
 
 module.exports.handler = async (event: any, context: any, callback: any) => {
@@ -10,7 +11,7 @@ module.exports.handler = async (event: any, context: any, callback: any) => {
   let res = {};
 
   try {
-    const requestInfo = request(event);
+    const requestInfo = httpRequest.init(event);
     console.log(requestInfo);
 
     switch (requestInfo.httpMethod) {
@@ -19,7 +20,7 @@ module.exports.handler = async (event: any, context: any, callback: any) => {
         if (requestInfo.path === '/test/product' || requestInfo.path === '/test/product/')
           res = await testListController.control(requestInfo.queryStringParameters);
         else if (requestInfo.path.startsWith('/test/product/') && requestInfo.pathParameters)
-          res = await testViewController(requestInfo.queryStringParameters, requestInfo.pathParameters);
+          res = await testViewController.control(requestInfo.queryStringParameters, requestInfo.pathParameters);
         break;
       case 'POST':
         /* /test */
@@ -30,8 +31,8 @@ module.exports.handler = async (event: any, context: any, callback: any) => {
         break;
     }
   } catch (error) {
-    errorResponse(event, error, callback);
+    httpResponse.errorResponse(event, error, callback);
   }
 
-  successResponse(event, res, callback);
+  httpResponse.successResponse(event, res, callback);
 };
