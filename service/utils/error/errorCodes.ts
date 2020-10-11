b9f80cc63840d104b00d1b8a39e7c49cbe6994d4
taeho_jeon <superduper8989@gmail.com>
2020-10-11 17:08:28 +0900

    import 도입

--- service/utils/error/errorCodes.ts
+++ service/utils/error/errorCodes.ts
@@ -1,4 +1,3 @@
-export {};
 const ValidationError: number = 4000 // validation 오류
 const MissingQueryParameter: number = 4001 // 쿼리 파라미터 누락
 const MissingRequiredData = {code: 4002, message: 'Request body cannot be null'} // 바디 데이터 누락
@@ -6,9 +5,4 @@ const UniqueValueDuplicated = {code: 4003, message: 'Unique value duplicated'} /
 const SyntaxError: number = 4004 // Syntax 에러
 const AWSError: number = 5000 // AWS 관련 에러
 
-module.exports.ValidationError = ValidationError;
-module.exports.MissingQueryParameter = MissingQueryParameter;
-module.exports.MissingRequiredData = MissingRequiredData;
-module.exports.UniqueValueDuplicated = UniqueValueDuplicated;
-module.exports.SyntaxError = SyntaxError;
-module.exports.AWSError = AWSError;
+export {ValidationError, MissingQueryParameter, MissingRequiredData, UniqueValueDuplicated, SyntaxError, AWSError}
--- service/utils/validation/schema.ts
+++ service/utils/validation/schema.ts
@@ -1,23 +1,26 @@
-export {};
 const Joi = require('@hapi/joi');
 
-module.exports.testSchema = Joi.object({
-    productName: Joi.string().min(1).max(50),
-    price: Joi.number().positive(),
-    categoryId: Joi.number().positive()
-});
+const schema = {
+    testSchema: Joi.object({
+        productName: Joi.string().min(1).max(50),
+        price: Joi.number().positive(),
+        categoryId: Joi.number().positive()
+    }),
 
-module.exports.productSchema = Joi.object({
-    productName: Joi.string().min(1).max(50),
-    price: Joi.number().positive(),
-    categoryId: Joi.number().positive()
-});
+    productSchema: Joi.object({
+        productName: Joi.string().min(1).max(50),
+        price: Joi.number().positive(),
+        categoryId: Joi.number().positive()
+    }),
 
-module.exports.productListSchema = Joi.object({
-    barcode: Joi.string().min(0).max(50).allow(''),
-    name: Joi.string().min(0).max(500).allow('')
-});
+    productListSchema: Joi.object({
+        barcode: Joi.string().min(0).max(50).allow(''),
+        name: Joi.string().min(0).max(500).allow('')
+    }),
 
-module.exports.userSchema = Joi.object({
-    principalId: Joi.string().min(1).max(50).required()
-});
+    userSchema: Joi.object({
+        principalId: Joi.string().min(1).max(50).required()
+    })
+};
+
+export default schema
