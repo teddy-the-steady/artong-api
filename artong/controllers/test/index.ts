5d5d93e1f2a151eb98a3eb375551ea426e1f6949
taeho_jeon <superduper8989@gmail.com>
2021-02-20 12:18:57 +0900

    small changes

---
+++ artong/controllers/test/index.ts
@@ -0,0 +1,5 @@
+module.exports = {
+    testCreateController: require('../test/testCreateController'),
+    testListController: require('../test/testListController'),
+    testViewController: require('../test/testViewController')
+}
