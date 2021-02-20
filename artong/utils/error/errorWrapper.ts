0fcc288e9e420b3a9d6e9e5461f0a0e9237daa47
taeho_jeon <superduper8989@gmail.com>
2021-02-20 19:41:58 +0900

    joi import bug

--- artong/utils/error/errorWrapper.ts
+++ artong/utils/error/errorWrapper.ts
@@ -8,11 +8,6 @@ module.exports.controllerErrorWrapper = function(error: any) {
         throw new BadRequest(error['errorMessage'], error['errorCode'])
     } else if (error instanceof InternalServerError) {
         throw new InternalServerError(error['errorMessage'], error['errorCode'])
-    } else if (error['details'] && error['details'][0]['message']) { // Joi Error
-        if (error['details'][0]['type'] === 'number.positive') {
-            throw new BadRequest(errorCodes.PositiveValueRequired.message, errorCodes.PositiveValueRequired.code)
-        }
-        throw new BadRequest(error['details'][0]['message'], errorCodes.MissingRequiredData.code)
     } else if (error instanceof Forbidden) {
         throw new Forbidden(error['errorMessage'], error['errorCode'])
     } else {
