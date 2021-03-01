0628c5f174e96773685dd89f49f748aa16739775
taeho_jeon <hohoworld11@gmail.com>
2021-03-01 11:50:32 +0900

    createMember master+detail, db error 추가 및 정리

--- artong/utils/error/errorWrapper.ts
+++ artong/utils/error/errorWrapper.ts
@@ -1,26 +1,27 @@
 import { BadRequest, InternalServerError, Forbidden } from './errors';
-import { UnknownError, ValidationError } from './errorCodes';
+import { DBError, UniqueConstraint, UnknownError, ValidationError, DBSyntaxError } from './errorCodes';
 
 const controllerErrorWrapper = function(error: any) {
 	console.log(error)
 	if (error instanceof BadRequest) {
 		switch (error.errorCode) {
-			case 4000:
-				if (process.env.ENV === 'PROD') {
-					throw new BadRequest(ValidationError.message, ValidationError.code);
-				} else {
-					throw new BadRequest(error.errorMessage, UnknownError.code);
-				}
-			default:
-				throw new BadRequest(error['errorMessage'], error['errorCode'])
+			case ValidationError.code:
+				if (process.env.ENV === 'prod') throw new BadRequest(ValidationError.message, ValidationError.code);
 		}
+		throw new BadRequest(error.errorMessage, error.errorCode);
 	} else if (error instanceof InternalServerError) {
-		throw new InternalServerError(error['errorMessage'], error['errorCode'])
+		switch (error.errorCode) {
+			case DBError.code:
+				if (process.env.ENV === 'prod') throw new InternalServerError(DBError.message, DBError.code);
+				if (error.errorMessage.code === '23505') throw new InternalServerError(error.errorMessage.detail, UniqueConstraint.code);
+				if (error.errorMessage.code === '42601') throw new InternalServerError(DBSyntaxError.message + ' at position: ' + error.errorMessage.position, DBSyntaxError.code);
+		}
+		throw new InternalServerError(error.errorMessage, error.errorCode);
 	} else if (error instanceof Forbidden) {
-		throw new Forbidden(error['errorMessage'], error['errorCode'])
+		throw new Forbidden(error.errorMessage, error.errorCode);
 	} else {
-		if (process.env.ENV === 'PROD') {
-			throw new InternalServerError(UnknownError.message, UnknownError.code)
+		if (process.env.ENV === 'prod') {
+			throw new InternalServerError(UnknownError.message, UnknownError.code);
 		} else {
 			throw new InternalServerError(error, UnknownError.code);
 		}
