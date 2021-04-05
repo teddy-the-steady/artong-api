ae83a11a567eae4e17418dd70531a652394e0cda
taeho_jeon <superduper8989@gmail.com>
2021-04-05 15:11:26 +0900

    small fixes

--- artong/models/member/selectMember.sql
+++ artong/models/member/selectMember.sql
@@ -1,7 +1,7 @@
 SELECT 
     m.*,
     d.*,
-    c.iso_code_2
+    c.iso_code_2 AS language
 FROM member_master m
 LEFT JOIN member_detail d ON d.member_id = m.id
 LEFT JOIN country c ON c.id = d.language_id
