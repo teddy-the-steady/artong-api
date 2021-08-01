81f415b9ea209633974e9a7af5bdc887dc565eb3
teddy <hohoworld11@gmail.com>
2021-08-01 23:42:52 +0900

    updateMemberProfilePic.sql 디버그

--- artong/models/member/updateMemberProfilePic.sql
+++ artong/models/member/updateMemberProfilePic.sql
@@ -3,4 +3,4 @@ SET
     updated_at = now()
     ,profile_pic = {{#if profile_pic}} '{{profile_pic}}' {{else if (or (eq profile_pic null) (eq profile_pic ''))}} NULL {{else}} profile_pic {{/if}}
     ,last_activity_at = {{#if last_activity_at}} now() {{else}} last_activity_at {{/if}}
-WHERE username = '{{username}}'
+WHERE member_id = (SELECT id FROM member_master WHERE username = '{{username}}')
