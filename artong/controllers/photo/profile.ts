65371ca3a3d72c1a251f4702ad984ce0c7e165cc
teddy <hohoworld11@gmail.com>
2021-08-01 15:12:48 +0900

    photo handler/controller 추가, PATCH //memberDetail/{username}/profilePic 추가, node_layer axios 추가, apiKeys 추가

---
+++ artong/controllers/photo/profile.ts
@@ -0,0 +1,21 @@
+import controllerErrorWrapper from '../../utils/error/errorWrapper';
+import axios from 'axios';
+axios.defaults.baseURL = 'https://6tz1h3qch8.execute-api.ap-northeast-2.amazonaws.com/stage/artong/v1';
+
+const updateProfilePic = async function(s3: any) {
+  try {    
+    const bucket = s3.bucket.name;
+    const key = decodeURIComponent(s3.object.key);
+    const username = key.split('/')[1];
+    const size = s3.object.size;
+
+    const member = await axios.get(`/member?username=${username}`);
+    console.log('GET /member?username', member);
+  } catch (error) {
+    controllerErrorWrapper(error);
+  }
+};
+
+export {
+	updateProfilePic,
+};
---
+++ artong/models/member/updateMemberProfilePic.sql
@@ -0,0 +1,6 @@
+UPDATE member_detail
+SET
+    updated_at = now()
+    ,profile_pic = {{#if profile_pic}} '{{profile_pic}}' {{else if (or (eq profile_pic null) (eq profile_pic ''))}} NULL {{else}} profile_pic {{/if}}
+    ,last_activity_at = {{#if last_activity_at}} now() {{else}} last_activity_at {{/if}}
+WHERE username = '{{username}}'
