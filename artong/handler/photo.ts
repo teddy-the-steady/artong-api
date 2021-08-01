1b3b9238cbf42b3b8633527743c4202b74a33e6e
taeho_jeon <superduper8989@gmail.com>
2021-08-01 16:53:37 +0900

    context.callbackWaitsForEmptyEventLoop = false 주석 / 오타 수정

--- artong/handler/photo.ts
+++ artong/handler/photo.ts
@@ -2,14 +2,14 @@ import { profile } from '../controllers/photo/index';
 import { InternalServerError } from '../utils/error/errors';
 
 export async function handler(event: any, context: any, callback: any) {
-  context.callbackWaitsForEmptyEventLoop = false;
-  
+  // context.callbackWaitsForEmptyEventLoop = false;
+
   try {
     const key = decodeURIComponent(event.Records[0].s3.object.key);
     const folder = key.split('/')[2];
 
     switch (folder) {
-      case 'pofile':
+      case 'profile':
         await profile.updateProfilePic(event.Records[0].s3);
         break;
       default:
