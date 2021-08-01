016a428116d98174d22c12b19745da66286b8b5a
teddy <teddy.jeon@speedwagon.io>
2021-08-01 16:45:12 +0900

    photo handler context.callbackWaitsForEmptyEventLoop = false 추가

--- artong/controllers/photo/profile.ts
+++ artong/controllers/photo/profile.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://6tz1h3qch8.execute-api.ap-northeast-2.amazonaws.com/stage/artong/v1';
 
 const updateProfilePic = async function(s3: any) {
-  try {    
+  try {
+    console.log('in updateProfilePic:', s3)
     const bucket = s3.bucket.name;
     const key = decodeURIComponent(s3.object.key);
     const username = key.split('/')[1];
