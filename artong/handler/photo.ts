36e9a5dc930fd11205e0f7176b94fcf5b08e4812
taeho_jeon <hohoworld11@gmail.com>
2021-08-01 22:22:54 +0900

    error handling

--- artong/handler/photo.ts
+++ artong/handler/photo.ts
@@ -1,5 +1,4 @@
 import { profile } from '../controllers/photo/index';
-import { InternalServerError } from '../utils/error/errors';
 
 export async function handler(event: any, context: any, callback: any) {
   // context.callbackWaitsForEmptyEventLoop = false;
@@ -17,7 +16,6 @@ export async function handler(event: any, context: any, callback: any) {
     }
     return {'data': 'success'};
   } catch (err) {
-    console.log(err);
-    throw new InternalServerError(err, null);
+    console.error(err);
   }
 };
