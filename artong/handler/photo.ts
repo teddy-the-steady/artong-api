a3bfd2e9d0d5c604ccf820705b0a0ff5b0198aa0
taeho_jeon <superduper8989@gmail.com>
2021-08-01 15:12:48 +0900

    photo handler/controller 추가, PATCH //memberDetail/{username}/profilePic 추가, node_layer axios 추가, apiKeys 추가

--- artong/handler/photo.ts
+++ artong/handler/photo.ts
@@ -1,26 +1,20 @@
-import 'reflect-metadata';
-import * as init from '../init';
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+import { profile } from '../controllers/photo/index';
 
 export async function handler(event: any, context: any, callback: any) {
-  console.log('Received event:', JSON.stringify(event, null, 2));
-
-  // Get the object from the event and show its content type
-  const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-  const params = {
-    Bucket: bucket,
-    Key: key,
-  }; 
   try {
-    const { ContentType } = await s3.getObject(params).promise();
-    console.log('CONTENT TYPE:', ContentType);
-    return ContentType;
-  } catch (err) {
+    const key = decodeURIComponent(event.Records[0].s3.object.key);
+    const folder = key.split('/')[2];
+
+    switch (folder) {
+      case 'pofile':
+        await profile.updateProfilePic(event.Records[0].s3);
+        break;
+      default:
+        break;
+    }
+    return {'data': 'success'};
+  } catch (err: any) {
     console.log(err);
-    const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
-    console.log(message);
-    throw new Error(message);
+    throw new Error(err);
   }
 };
