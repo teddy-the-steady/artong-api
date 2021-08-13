1a31164ebf34a4a9b9831f2ce60332fbf9110a04
teddy <teddy.jeon@speedwagon.io>
2021-08-13 23:34:37 +0900

    save only key from s3

--- artong/handler/photo.ts
+++ artong/handler/photo.ts
@@ -1,14 +1,16 @@
 import { profile } from '../controllers/photo/index';
 import { InternalServerError } from '../utils/error/errors';
 
+// TODO] axios baseUrl setting 여기서?
+// x-api-key ssm에서 가져오기?
 export async function handler(event: any, context: any, callback: any) {
-  // context.callbackWaitsForEmptyEventLoop = false;
+  console.log(event);
 
   try {
     const key = decodeURIComponent(event.Records[0].s3.object.key);
-    const folder = key.split('/')[2];
+    const type = key.split('/')[2];
 
-    switch (folder) {
+    switch (type) {
       case 'profile':
         await profile.updateProfilePic(event.Records[0].s3);
         break;
