208bf769f02ed077d4f44bc9aeedb11c92185f0c
taeho_jeon <superduper8989@gmail.com>
2021-12-14 13:32:43 +0900

    insert upload and contents at once

--- artong/models/uploads/insertUpload.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO 
-  uploads(member_id, description, thumbnail_url)
-VALUES(
-  (SELECT id FROM member_master WHERE username = '{{username}}'),
-  '{{description}}',
-  '{{thumbnail_url}}'
-)
-RETURNING id
