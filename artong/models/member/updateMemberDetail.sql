9e27222943b41992b1583d0db34113b84dc81cb3
teddy <hohoworld11@gmail.com>
2022-01-09 13:09:41 +0900

    fix handler logic, last_activity_at 컬럼 제거

--- artong/models/member/updateMemberDetail.sql
+++ artong/models/member/updateMemberDetail.sql
@@ -10,7 +10,6 @@ SET
     ,introduction = {{#if introduction}} '{{introduction}}' {{else if (or (eq introduction null) (eq introduction ''))}} NULL {{else}} introduction {{/if}}
     ,profile_pic = {{#if profile_pic}} '{{profile_pic}}' {{else if (or (eq profile_pic null) (eq profile_pic ''))}} NULL {{else}} profile_pic {{/if}}
     ,language_id = {{#if language_id}} {{language_id}} {{else if (eq language_id null)}} NULL {{else}} language_id {{/if}}
-    ,last_activity_at = {{#if last_activity_at}} now() {{else}} last_activity_at {{/if}}
     ,phone_number = {{#if phone_number}} '{{phone_number}}' {{else if (or (eq phone_number null) (eq phone_number ''))}} NULL {{else}} phone_number {{/if}}
     ,country_id = {{#if country_id}} {{country_id}} {{else if (eq country_id null)}} NULL {{else}} country_id {{/if}}
 WHERE member_id = {{member_id}} AND {{member_id}} = (SELECT id FROM member_master WHERE auth_id = '{{auth_id}}')
