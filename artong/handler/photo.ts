a5d8914e0cff87627afdae96bbd253bbeead24e5
teddy <hohoworld11@gmail.com>
2022-01-31 16:19:45 +0900

    initialize setApiKey

--- artong/handler/photo.ts
+++ artong/handler/photo.ts
@@ -4,31 +4,35 @@ import getSecretKeys from '../utils/common/ssmKeys';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://6tz1h3qch8.execute-api.ap-northeast-2.amazonaws.com/stage/artong/v1';
-axios.defaults.headers.common['x-api-key'] = (async function() {
+const setApiKey = async function() {
   const keys = await getSecretKeys();
   console.log(keys)
-  return keys['/apikey/artongApiKeyStage'];
-})();
-console.log(axios.defaults)
+  axios.defaults.headers.common['x-api-key'] = keys['/apikey/artongApiKeyStage'];
+};
+let initKeys: any = null;
 
 export async function handler(event: any, context: any, callback: any) {
   console.log(event);
 
   try {
-    const key = decodeURIComponent(event.Records[0].s3.object.key);
-    const type = key.split('/')[2];
+    if (!initKeys) {
+      initKeys = await setApiKey();
+    } else {
+      const key = decodeURIComponent(event.Records[0].s3.object.key);
+      const type = key.split('/')[2];
 
-    switch (type) {
-      case 'profile':
-        await profile.updateProfilePic(event.Records[0].s3);
-        break;
-      case 'contents':
-        await contents.createUploadAndContent(event.Records[0].s3);
-        break;
-      default:
-        break;
+      switch (type) {
+        case 'profile':
+          await profile.updateProfilePic(event.Records[0].s3);
+          break;
+        case 'contents':
+          await contents.createUploadAndContent(event.Records[0].s3);
+          break;
+        default:
+          break;
+      }
+      return {'data': 'success'};
     }
-    return {'data': 'success'};
   } catch (err) {
     throw new InternalServerError(err, null);
   }
