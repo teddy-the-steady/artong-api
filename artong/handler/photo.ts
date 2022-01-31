63d52a5645e384b30526bf3667601071508e503f
teddy <teddy.jeon@speedwagon.io>
2022-01-31 17:08:55 +0900

    initialization 수정

--- artong/handler/photo.ts
+++ artong/handler/photo.ts
@@ -6,10 +6,9 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://6tz1h3qch8.execute-api.ap-northeast-2.amazonaws.com/stage/artong/v1';
 const setApiKey = async function() {
   const keys = await getSecretKeys();
-  console.log(keys)
   axios.defaults.headers.common['x-api-key'] = keys['/apikey/artongApiKeyStage'];
 };
-let initKeys: any = null;
+var initKeys: any = null;
 
 export async function handler(event: any, context: any, callback: any) {
   console.log(event);
@@ -17,22 +16,21 @@ export async function handler(event: any, context: any, callback: any) {
   try {
     if (!initKeys) {
       initKeys = await setApiKey();
-    } else {
-      const key = decodeURIComponent(event.Records[0].s3.object.key);
-      const type = key.split('/')[2];
+    } 
+    const key = decodeURIComponent(event.Records[0].s3.object.key);
+    const type = key.split('/')[2];
 
-      switch (type) {
-        case 'profile':
-          await profile.updateProfilePic(event.Records[0].s3);
-          break;
-        case 'contents':
-          await contents.createUploadAndContent(event.Records[0].s3);
-          break;
-        default:
-          break;
-      }
-      return {'data': 'success'};
+    switch (type) {
+      case 'profile':
+        await profile.updateProfilePic(event.Records[0].s3);
+        break;
+      case 'contents':
+        await contents.createUploadAndContent(event.Records[0].s3);
+        break;
+      default:
+        break;
     }
+    return {'data': 'success'};
   } catch (err) {
     throw new InternalServerError(err, null);
   }
