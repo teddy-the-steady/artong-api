3412aac45e95c9c028e12cfdcfb49c48e4fbe661
teddy <teddy.jeon@speedwagon.io>
2022-03-05 13:17:19 +0900

    sql 수정

--- artong/models/uploads/selectAuthUserUploads.sql
+++ artong/models/uploads/selectAuthUserUploads.sql
@@ -1,4 +1,8 @@
-/*+ IndexScan(uploads uploads_pkey) */
+/*+
+    IndexScan(u uploads_pkey)
+    IndexScan(m member_master_pkey)
+    IndexScan(d member_detail_member_id_idx)
+*/
 SELECT
     u.*, d.profile_pic, m.username
     ,(SELECT
@@ -8,8 +12,8 @@ SELECT
 	AND ua.member_id = ${member_id}
     ORDER BY updated_at DESC LIMIT 1) AS "like"
 FROM uploads u
-LEFT OUTER JOIN member_master m ON u.member_id = m.id
-LEFT OUTER JOIN member_detail d ON u.member_id = d.member_id
+LEFT JOIN member_master m ON u.member_id = m.id
+LEFT JOIN member_detail d ON u.member_id = d.member_id
  WHERE
     1 = 1
 {{#exists username}}
