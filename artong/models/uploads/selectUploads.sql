6da16e0d3f0c2a59c645ca3589a4a24711804f76
teddy <hohoworld11@gmail.com>
2022-03-05 13:17:19 +0900

    sql 수정

--- artong/models/uploads/selectUploads.sql
+++ artong/models/uploads/selectUploads.sql
@@ -1,9 +1,13 @@
-/*+ IndexScan(uploads uploads_pkey) */
+/*+
+    IndexScan(u uploads_pkey)
+    IndexScan(m member_master_pkey)
+    IndexScan(d member_detail_member_id_idx)
+*/
 SELECT
     u.*, d.profile_pic, m.username
 FROM uploads u
-LEFT OUTER JOIN member_master m ON u.member_id = m.id
-LEFT OUTER JOIN member_detail d ON u.member_id = d.member_id
+LEFT JOIN member_master m ON u.member_id = m.id
+LEFT JOIN member_detail d ON u.member_id = d.member_id
  WHERE
     1 = 1
 {{#exists username}}
