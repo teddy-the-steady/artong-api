733a3f1e3c771e41e8dec6970ce1c287db5224c0
teddy <teddy.jeon@speedwagon.io>
2022-04-23 10:00:25 +0900

    webpack.config.js check ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
+const isLocal = slsw.lib.webpack.isLocal;
+const ENV = slsw.lib.serverless.service.provider.environment
 const { IgnorePlugin } = require('webpack');
 
 module.exports = {
-  mode: slsw.lib.webpack.isLocal || process.env.ENV === 'stage' ? 'development' : 'production',
-  entry: slsw.lib.webpack.isLocal ? {
+  mode: isLocal || ENV !== 'prod' ? 'development' : 'production',
+  entry: isLocal ? {
     artong: path.join(__dirname, 'artong/handler/artong.ts'),
   } : null,
-  output: slsw.lib.webpack.isLocal ? {
+  output: isLocal ? {
     libraryTarget: 'commonjs',
     filename: 'artong/handler/[name].js',
     path: path.resolve(__dirname, 'dist')
