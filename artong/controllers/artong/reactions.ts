559a8e39c2eb18758d133dc45c4d336f1d3ef873
taeho_jeon <superduper8989@gmail.com>
2022-06-08 15:20:04 +0900

    req data interface as Info

--- artong/controllers/artong/reactions.ts
+++ artong/controllers/artong/reactions.ts
@@ -1,17 +1,24 @@
 import { ContentReactions } from '../../models/index';
 
+interface ContentReactionsInfo {
+  reaction_id: number;
+	content_id: number;
+	member_id: number;
+}
+
 const createContentReaction = async function(pathParameters: any, body: any, user: any) {
-  const reaction = new ContentReactions({
+  const reaction = new ContentReactions();
+
+  const result = await reaction.createContentReaction({
     reaction_id: body.reaction_code,
     content_id: pathParameters.id,
     member_id: user.member_id
   });
 
-  const result = await reaction.createContentReaction();
-
   return {'data': result}
 }
 
 export {
+  ContentReactionsInfo,
   createContentReaction,
 };
