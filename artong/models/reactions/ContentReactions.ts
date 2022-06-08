b273d8e022b298bf15642d15edbb9ab63489ebb8
teddy <teddy.jeon@speedwagon.io>
2022-06-08 13:13:01 +0900

    ContentReactions typescript mvc

--- artong/models/reactions/ContentReactions.ts
+++ artong/models/reactions/ContentReactions.ts
@@ -1,17 +1,47 @@
+import * as db from '../../utils/db/db';
+import controllerErrorWrapper from '../../utils/error/errorWrapper';
+const insertContentReactions = require('../../models/reactions/insertContentReactions.sql')
+
 export default class ContentReactions {
-    reaction_id: number;
-    content_id: number;
-    member_id: number;
+	reaction_id: number | null;
+	content_id: number | null;
+	member_id: number | null;
 
-    created_at: Date;
-    updated_at: Date;
+	created_at: Date | null;
+	updated_at: Date | null;
 
-    constructor(obj: any) {
-        this.reaction_id = obj.reaction_id;
-        this.content_id = obj.content_id;
-        this.member_id = obj.member_id;
+	constructor({
+		reaction_id = null,
+		content_id = null,
+		member_id = null,
+		created_at = null,
+		updated_at = null
+	} = {}) {
+		this.reaction_id = reaction_id;
+		this.content_id = content_id;
+		this.member_id = member_id;
 
-        this.created_at = obj.created_at;
-        this.updated_at = obj.updated_at;
-    }
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+	}
+
+	async createContentReaction() {
+		let conn: any;
+
+		try {
+			conn = await db.getConnection();
+			await db.beginTransaction(conn);
+
+			const result = await db.execute(conn, insertContentReactions, this);
+
+			await db.commit(conn);
+
+			return result
+		} catch (error) {
+			if (conn) await db.rollBack(conn);
+			controllerErrorWrapper(error);
+		} finally {
+			if (conn) db.release(conn);
+		}
+	}
 }
