26e77330b71abc4b74b50dd6840b2fc1da9eea64
teddy <hohoworld11@gmail.com>
2022-06-08 13:18:50 +0900

    from upsert to only insert content_reactions

--- artong/models/reactions/insertContentReactions.sql
+++ artong/models/reactions/insertContentReactions.sql
@@ -4,7 +4,5 @@ VALUES(
 	(SELECT id FROM reactions WHERE code = UPPER(${reaction_id})),
 	${content_id},
 	${member_id}
-) ON CONFLICT (reaction_id, content_id, member_id) DO
-UPDATE SET
-	reaction_id = (SELECT id FROM reactions WHERE code = UPPER(${reaction_id})),
-	updated_at = now()
+)
+RETURNING reaction_id, content_id, member_id
