d930eadd00c1d640e2705009849b2446b4c84dac
teddy <hohoworld11@gmail.com>
2022-06-08 13:27:53 +0900

    status 제거

--- artong/models/index.ts
+++ artong/models/index.ts
@@ -1,13 +1,11 @@
 import MemberMaster from './member/MemberMaster';
 import MemberDetail from './member/MemberDetail';
-import Status from './status/Status';
 import Country from './country/Country';
 import ContentReactions from './reactions/ContentReactions';
 
 export {
 	MemberMaster,
 	MemberDetail,
-	Status,
 	Country,
 	ContentReactions,
 };
