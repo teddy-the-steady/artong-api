567179f67b5cc1042ca436410e8d8f6519cb7602
teddy <teddy.jeon@speedwagon.io>
2022-09-08 11:53:05 +0900

    POST /projects added

--- artong/models/index.ts
+++ artong/models/index.ts
@@ -1,9 +1,11 @@
 import { Member } from './member/Member';
 import { Country } from './country/Country';
-import ContentReactions from './reactions/ContentReactions';
+import { ContentReactions } from './reactions/ContentReactions';
+import { Projects } from './projects/Projects';
 
 export {
 	Member,
 	Country,
 	ContentReactions,
+	Projects,
 };
