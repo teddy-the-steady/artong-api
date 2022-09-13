1695a8700487fbea343690440f95e3ccd940f9ad
taeho_jeon <hohoworld11@gmail.com>
2022-09-13 16:40:47 +0900

    Projects model create_tx_hash as id

--- artong/handler/artong.ts
+++ artong/handler/artong.ts
@@ -32,7 +32,7 @@ export async function handler(event: any, context: any, callback: any) {
         if (req.path.startsWith('/artong/v1/members/') && req.pathParameters && req.path.includes('profile_pic'))
           res = await member.patchMemberProfilePic(req.pathParameters, req.body);
         else if (req.path.startsWith('/artong/v1/projects/') && req.pathParameters)
-          res = await projects.patchProject(req.pathParameters, req.body);
+          res = await projects.patchProject(req.pathParameters, req.body, req.member);
         break;
       default:
         console.error('METHOD undefined');
