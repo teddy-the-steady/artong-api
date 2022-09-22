748bf5f82a0b702b14f224673c079528004b8033
taeho_jeon <superduper8989@gmail.com>
2022-09-22 14:45:55 +0900

    added symbol to Projects model

--- artong/models/projects/insertProject.sql
+++ artong/models/projects/insertProject.sql
@@ -1,3 +1,3 @@
-INSERT INTO projects (create_tx_hash, member_id, name, status)
-VALUES(${create_tx_hash}, ${member_id}, ${name}, ${status})
+INSERT INTO projects (create_tx_hash, member_id, name, symbol, status)
+VALUES(${create_tx_hash}, ${member_id}, ${name}, ${symbol}, ${status})
 RETURNING *
