f68eaedf6544ea4c24ebd6aa766780db341c0f46
teddy <hohoworld11@gmail.com>
2022-11-03 16:55:57 +0900

    code clearing

--- artong/utils/db/db.ts
+++ artong/utils/db/db.ts
@@ -29,7 +29,7 @@ const execute = async function(conn: PoolClient, sql: string, params: any) {
   const convertedSql = queryConverter(compiledModel, params);
   /* INFO] 쿼리 debug시 주석 해제 */
   // console.log(compiledModel);
-  console.log(convertedSql);
+  // console.log(convertedSql);
   try {
     const result = await conn.query(convertedSql);
     return result['rows']
