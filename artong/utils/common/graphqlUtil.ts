8d9d13d796c87d74b30397b63de2b327053c375d
taeho_jeon <hohoworld11@gmail.com>
2022-12-01 22:33:13 +0900

    abi updated, new contract address, subgraph v0.0.23

--- artong/utils/common/graphqlUtil.ts
+++ artong/utils/common/graphqlUtil.ts
@@ -3,7 +3,7 @@ import { BadRequest } from '../error/errors';
 
 const graphqlRequest = async function(body: any) {
   const result = await axios({
-    url: '/query/36284/artong-test/v0.0.23',
+    url: '/query/36284/artong-test/v0.0.24',
     baseURL: 'https://api.studio.thegraph.com',
     method: 'POST',
     data: body
