d6a0513afcdc7fdb1ab0d955a7c5c404d37e9df8
teddy <hohoworld11@gmail.com>
2022-12-07 16:45:20 +0900

    selectProjectContributors.sql select fields

--- artong/models/member/selectProjectContributors.sql
+++ artong/models/member/selectProjectContributors.sql
@@ -1,6 +1,15 @@
 SELECT
     DISTINCT c.member_id,
-    m.*,
+    m.email,
+    m.username,
+    m.introduction,
+    m.profile_s3key,
+    m.profile_thumbnail_s3key,
+    m.country_id,
+    m.wallet_address,
+    m.principal_id,
+    m.created_at,
+    m.updated_at,
     COUNT(c.member_id) as contributions
 FROM contents c
 JOIN member m ON m.id = c.member_id
