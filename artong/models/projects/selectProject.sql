9ff7226f112df886c449665a0673d63f15977bb0
taeho_jeon <superduper8989@gmail.com>
2022-12-12 17:15:23 +0900

    added subscribers in selectProject.sql

--- artong/models/projects/selectProject.sql
+++ artong/models/projects/selectProject.sql
@@ -1,7 +1,8 @@
 SELECT
-  *
+  p.*,
+  (SELECT COUNT(*) FROM subscribe s WHERE s.project_address = p.address) AS subscribers
 FROM
-  projects
+  projects p
 WHERE
   {{#exists create_tx_hash}} create_tx_hash = ${create_tx_hash} {{/exists}}
   {{#exists address}} address = ${address} {{/exists}}
