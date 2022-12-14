82db50d1118fbea2540ef2381daa3b85c6e4636d
taeho_jeon <hohoworld11@gmail.com>
2022-12-14 20:05:16 +0900

    added project_s3key/background_s3key in postProject

--- artong/models/projects/insertProject.sql
+++ artong/models/projects/insertProject.sql
@@ -1,3 +1,3 @@
-INSERT INTO projects (create_tx_hash, member_id, name, symbol, status)
-VALUES(${create_tx_hash}, ${member_id}, ${name}, ${symbol}, ${status})
+INSERT INTO projects (create_tx_hash, member_id, name, symbol, status, project_s3key ,background_s3key)
+VALUES(${create_tx_hash}, ${member_id}, ${name}, ${symbol}, ${status}, ${project_s3key}, ${background_s3key})
 RETURNING *
