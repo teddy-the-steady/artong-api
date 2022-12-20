ad4d54517cf8d943d76b7a135d30e8b75338036a
taeho_jeon <hohoworld11@gmail.com>
2022-12-20 10:25:29 +0900

    added price in queryTokenHistory

--- artong/models/contentsHistory/insertContentsHistories.sql
+++ artong/models/contentsHistory/insertContentsHistories.sql
@@ -1,5 +1,5 @@
 INSERT INTO contents_history
-    (history_type, subgraph_raw, tx_hash, from_member_id, to_member_id, block_timestamp, contents_id)
+    (history_type, subgraph_raw, tx_hash, from_member_id, to_member_id, price, block_timestamp, contents_id)
 VALUES
     {{#each histories}}
       (
@@ -13,6 +13,7 @@ VALUES
         '{{this.tx_hash}}',
         (SELECT id FROM member WHERE wallet_address = '{{this.from_member_id}}'),
         (SELECT id FROM member WHERE wallet_address = '{{this.to_member_id}}'),
+        '{{this.price}}',
         '{{this.block_timestamp}}',
         (SELECT id FROM contents WHERE project_address = ${project_address} AND token_id = ${token_id})
       )
