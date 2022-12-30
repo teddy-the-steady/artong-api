215bad8a1ece48bf8ec203d217655a88f1a3437c
taeho_jeon <superduper8989@gmail.com>
2022-12-30 10:24:33 +0900

    added max_token_id in queryProject

--- artong/models/projects/selectProject.sql
+++ artong/models/projects/selectProject.sql
@@ -1,6 +1,7 @@
 SELECT
   p.*,
-  (SELECT COUNT(*) FROM subscribe s WHERE s.project_address = p.address) AS subscribers
+  (SELECT COUNT(*) FROM subscribe s WHERE s.project_address = p.address) AS subscribers,
+  (SELECT MAX(token_id) FROM contents c WHERE c.project_address = p.address) AS max_token_id
 FROM
   projects p
 WHERE
