4ec55043a969667ba7a9d9358a239a2ee5020bcc
teddy <hohoworld11@gmail.com>
2023-01-04 11:34:13 +0900

    added is_contributor in queryProject

--- artong/models/projects/selectProject.sql
+++ artong/models/projects/selectProject.sql
@@ -3,6 +3,7 @@ SELECT
   (SELECT COUNT(*) FROM subscribe s WHERE s.project_address = p.address) AS subscribers,
   {{#exists member_id}}
     EXISTS(SELECT s.member_id FROM subscribe s WHERE s.project_address = p.address AND s.member_id = ${member_id}) AS is_subscriber,
+    EXISTS(SELECT c.member_id FROM contents c WHERE c.project_address = p.address AND c.token_id > 0 AND c.member_id = ${member_id}) AS is_contributor,
   {{/exists}}
   (SELECT MAX(token_id) FROM contents c WHERE c.project_address = p.address) AS max_token_id
 FROM
