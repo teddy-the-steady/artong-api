ca3e37b781e2fc226d09558538f373b566183111
taeho_jeon <hohoworld11@gmail.com>
2023-01-19 15:24:34 +0900

    added total_likes to postContentReaction

--- artong/models/reactions/ContentReactions.ts
+++ artong/models/reactions/ContentReactions.ts
@@ -2,6 +2,7 @@ import { PoolClient } from 'pg';
 import * as db from '../../utils/db/db';
 import Models from '../Models';
 const insertContentReactions = require('./insertContentReactions.sql')
+const selectTotalLikesByContent = require('./selectTotalLikesByContent.sql')
 
 class ContentReactions extends Models {
 	reaction_id?: number;
@@ -26,6 +27,19 @@ class ContentReactions extends Models {
 				reaction_code,
 				content_id,
 				member_id
+			});
+			return result[0]
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async getTotalLikesByContent(
+		content_id?: number,
+	): Promise<{ total_likes: string }> {
+		try {
+			const result = await db.execute(this.conn, selectTotalLikesByContent, {
+				content_id,
 			});
 			return result[0]
 		} catch (error) {
