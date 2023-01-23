9456befda1084dcc5210f4e89b8c943d77514f84
taeho_jeon <hohoworld11@gmail.com>
2023-01-23 14:44:06 +0900

    getMemberContentsFavorites with proper owner/creator

--- artong/models/member/selectMembersWithWalletAddressArray.sql
+++ artong/models/member/selectMembersWithWalletAddressArray.sql
@@ -1,5 +1,12 @@
 SELECT
-    m.*
+    m.id AS member_id,
+    m.email,
+    m.username,
+    m.profile_s3key,
+    m.profile_thumbnail_s3key,
+    m.wallet_address,
+    m.created_at AS member_created_at,
+    m.updated_at AS member_updated_at
 FROM member m
 WHERE 1=1
     AND wallet_address IN (
