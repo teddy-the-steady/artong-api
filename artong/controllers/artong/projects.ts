e3f8a3585f85dfb868e0183b0c43b7acb1bdc273
teddy <hohoworld11@gmail.com>
2023-01-31 12:47:12 +0900

    variable renamed, contributors added to GET /projects

--- artong/controllers/artong/projects.ts
+++ artong/controllers/artong/projects.ts
@@ -26,6 +26,22 @@ const getProjectsPrevNext = async function(queryStringParameters: GetProjectsInf
       projectModel.address,
       queryStringParameters.prev_next_count,
     );
+
+    const extractedProjectIds = result.map((project: Projects) => project.address) as string[];
+
+    const memberModel = new Member({}, conn);
+    const contributorsResult = await memberModel.getTopContributorsInProjects(extractedProjectIds);
+
+    if (contributorsResult.length > 0) {
+      const contributorsArrayGroupByProjectAddress = _.groupBy(contributorsResult as any[], c => c.project_address);
+      for (let project of result) {
+        if (contributorsArrayGroupByProjectAddress[project.address as string]) {
+          contributorsArrayGroupByProjectAddress[project.address as string].splice(5)
+        }
+        (project as any).contributors = contributorsArrayGroupByProjectAddress[project.address as string] || [];
+      }
+    }
+
     return {data: result}
   } catch (error) {
     throw controllerErrorWrapper(error);
@@ -265,7 +281,7 @@ const queryProjects = async function(body: any, _db_: string[], pureQuery: strin
     }
 
     const memberModel = new Member({}, conn);
-    const contributorsResult = await memberModel.getTop5ContributorsInProjects(extractedProjectIds);
+    const contributorsResult = await memberModel.getTopContributorsInProjects(extractedProjectIds);
 
     if (contributorsResult.length > 0) {
       const contributorsArrayGroupByProjectAddress = _.groupBy(contributorsResult as any[], c => c.project_address);
@@ -327,7 +343,7 @@ const queryProjectsByCreator = async function(body: any, _db_: string[], pureQue
     }
 
     const memberModel = new Member({}, conn);
-    const contributorsResult = await memberModel.getTop5ContributorsInProjects(extractedProjectIds);
+    const contributorsResult = await memberModel.getTopContributorsInProjects(extractedProjectIds);
 
     if (contributorsResult.length > 0) {
       const contributorsArrayGroupByProjectAddress = _.groupBy(contributorsResult as any[], c => c.project_address);
