c8e92db2514e6eb6e21e469433343b64c416176c
teddy <hohoworld11@gmail.com>
2023-02-12 12:54:18 +0900

    fixes on getMemberFollowerOrFollowing, POST /reports

--- artong/models/reports/insertReport.sql
+++ artong/models/reports/insertReport.sql
@@ -1,7 +1,17 @@
 INSERT INTO
-	reports(description, category, member_id)
+	reports(
+		{{#exists description}} description, {{/exists}}
+		{{#exists project_address}} project_address, {{/exists}}
+		{{#exists contents_id}} contents_id, {{/exists}}
+		{{#exists member_id_reported}} member_id_reported, {{/exists}}
+		category,
+		member_id
+	)
 VALUES(
-	${description},
+	{{#exists description}} ${description}, {{/exists}}
+	{{#exists project_address}} ${project_address}, {{/exists}}
+	{{#exists contents_id}} contents_id, {{/exists}}
+	{{#exists member_id_reported}} member_id_reported, {{/exists}}
 	${category},
 	${member_id}
 )
