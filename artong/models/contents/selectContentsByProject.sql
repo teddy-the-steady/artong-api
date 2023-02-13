a4f5e9574280033dca3916966d669fd869813e57
teddy <teddy.jeon@speedwagon.io>
2023-02-13 14:12:36 +0900

    status=APPROVED with is_redeemed IS NOT NULL

--- artong/models/contents/selectContentsByProject.sql
+++ artong/models/contents/selectContentsByProject.sql
@@ -20,7 +20,7 @@ WHERE 1=1
   AND project_address = ${project_address}
   AND (
     c.token_id > 0 OR
-    c.status = 'APPROVED'
+    (c.is_redeemed IS NOT NULL AND c.status = 'APPROVED')
   )
 
 {{#exists order_by}}
