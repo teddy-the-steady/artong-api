304a9a96f2b7c83dd508453199bca08ed1ac5737
teddy <hohoworld11@gmail.com>
2023-02-24 15:57:17 +0900

    prod main contents

--- artong/controllers/artong/main.ts
+++ artong/controllers/artong/main.ts
@@ -21,21 +21,14 @@ const getTop10Contributors = async function() {
 const getMainContents = function() {
   const result = {
     mainToken: {
-      project_address: '0xffcc9cbf76ddbb240ba8e614e10f3863ba10a8e4',
+      project_address: '0x9579daec07b696ffcd0b36e62266684aec9d02e8',
       token_id: '1'
     },
     highlightedProjects: [
-      '0xffcc9cbf76ddbb240ba8e614e10f3863ba10a8e4',
-      '0x9c202218148cfdff5e1e18f0c6a30d011de5c1ac',
-      '0x52234011cf229f16bbd2ff191c2627275de87870',
-      '0x6b426ca2327c72089d192bbf3eb43abfc438975d',
-      '0x0670eec8053e401c4bb5e89fb70be8508f01a054',
+      '0x9579daec07b696ffcd0b36e62266684aec9d02e8',
     ],
     artongsPick: [
-      '0x0670eec8053e401c4bb5e89fb70be8508f01a0541',
-      '0xffcc9cbf76ddbb240ba8e614e10f3863ba10a8e41',
-      '0x6b426ca2327c72089d192bbf3eb43abfc438975d1',
-      '0xffcc9cbf76ddbb240ba8e614e10f3863ba10a8e42',
+      '0x9579daec07b696ffcd0b36e62266684aec9d02e81',
     ]
   }
   return {data: result}
