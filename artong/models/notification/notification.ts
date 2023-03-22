98721e372fe8da4712f3e3066b3607cfa135618b
k3srpn <k3srpn@gmail.com>
2023-03-23 00:19:58 +0900

    feat(notification): 알림생성 Model 구현

--- artong/models/notification/notification.ts
+++ artong/models/notification/notification.ts
@@ -1,11 +1,16 @@
 import { PoolClient } from "pg";
 import Models from "../Models";
+import * as db from "../../utils/db/db";
+const insertNotification = require("./insertNotification.sql");
 
+type NotificationCategory = "like";
 export class Notification extends Models {
   id?: number;
+  category!: NotificationCategory;
   sender_id?: number;
   receiver_id?: number;
   read_at?: Date;
+  redirect_on_click?: string;
   content?: string;
   created_at?: Date;
   updated_at?: Date;
@@ -13,5 +18,26 @@ export class Notification extends Models {
   constructor(data: Partial<Notification> = {}, conn: PoolClient) {
     super(conn);
     Object.assign(this, data);
+  }
+
+  async createNotification(
+    category: NotificationCategory,
+    sender_id: number,
+    receiver_id: number,
+    content: string,
+    redirect_on_click?: string
+  ) {
+    try {
+      const result = await db.execute(this.conn, insertNotification, {
+        category,
+        sender_id,
+        receiver_id,
+        content,
+        redirect_on_click,
+      });
+      return result[0];
+    } catch (error) {
+      throw error;
+    }
   }
 }
