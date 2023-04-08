10d73c320540a42b5ba241cf6721c59b1a4d5259
k3srpn <k3srpn@gmail.com>
2023-04-08 20:48:50 +0900

    dump

--- artong/models/notification/Notification.ts
+++ artong/models/notification/Notification.ts
@@ -24,7 +24,7 @@ class Notification extends Models {
   content?: string;
   created_at?: Date;
   updated_at?: Date;
-  //
+  
   constructor(data: Partial<Notification> = {}, conn: PoolClient) {
     super(conn);
     Object.assign(this, data);
