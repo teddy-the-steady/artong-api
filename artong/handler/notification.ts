43b5e12da13ef80c22021ff8e365e7778554f701
k3srpn <k3srpn@gmail.com>
2023-04-08 22:21:48 +0900

    dummy

--- artong/handler/notification.ts
+++ artong/handler/notification.ts
@@ -14,6 +14,6 @@ export async function handler(event: SQSEvent) {
   //   const message = record.body
   //   // const receiveMessage = notificationModel.recvMessage(message)
   //   console.log('receiveMessage===>',message)
-    
+    //
   // }
 }
