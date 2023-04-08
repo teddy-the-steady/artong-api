40f5aaabf73e168da4e35b2ffcfda1451cd4f4ce
k3srpn <k3srpn@gmail.com>
2023-04-08 18:49:16 +0900

    feat: notification sqs trigger handler 구현

---
+++ artong/handler/notification.ts
@@ -0,0 +1,19 @@
+import { SQSEvent } from 'aws-lambda';
+import { Pool, PoolClient } from 'pg';
+import * as db from '../utils/db/db';
+import { Notification } from '../models';
+import { getDbConnentionPool } from '../init';
+export let dbConnectionPool: Pool;
+
+export async function handler(event: SQSEvent) {
+  // dbConnectionPool = await getDbConnentionPool();
+  // const conn: PoolClient = await db.getConnection();
+  // const notificationModel = new Notification({},conn)
+  for (const record of event.Records) {
+    // const message = JSON.parse(record.body)
+    const message = record.body
+    // const receiveMessage = notificationModel.recvMessage(message)
+    console.log('receiveMessage===>',message)
+    
+  }
+}
