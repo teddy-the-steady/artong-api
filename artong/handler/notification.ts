43529c8c8461c7e0afe4bf63d161c42fd105cbb5
k3srpn <k3srpn@gmail.com>
2023-04-08 20:18:01 +0900

    fix: 메모리누수 발생 코드 주석

--- artong/handler/notification.ts
+++ artong/handler/notification.ts
@@ -9,11 +9,11 @@ export async function handler(event: SQSEvent) {
   // dbConnectionPool = await getDbConnentionPool();
   // const conn: PoolClient = await db.getConnection();
   // const notificationModel = new Notification({},conn)
-  for (const record of event.Records) {
-    // const message = JSON.parse(record.body)
-    const message = record.body
-    // const receiveMessage = notificationModel.recvMessage(message)
-    console.log('receiveMessage===>',message)
+  // for (const record of event.Records) {
+  //   // const message = JSON.parse(record.body)
+  //   const message = record.body
+  //   // const receiveMessage = notificationModel.recvMessage(message)
+  //   console.log('receiveMessage===>',message)
     
-  }
+  // }
 }
