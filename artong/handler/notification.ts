e4a89c77a940a0d84040e2d90edcc6518351854c
k3srpn <k3srpn@gmail.com>
2023-04-13 22:04:09 +0900

    feat(noti): handler init

--- artong/handler/notification.ts
+++ artong/handler/notification.ts
@@ -2,17 +2,16 @@ import { Pool, PoolClient } from 'pg';
 import * as db from '../utils/db/db';
 import { Notification } from '../models';
 import { getDbConnentionPool } from '../init';
-export let dbConnectionPool: Pool;
+
+let dbConnectionPool: Pool;
 
 export async function handler(event: any) {
-  // dbConnectionPool = await getDbConnentionPool();
-  // const conn: PoolClient = await db.getConnection();
-  // const notificationModel = new Notification({},conn)
-  // for (const record of event.Records) {
-  //   // const message = JSON.parse(record.body)
-  //   const message = record.body
-  //   // const receiveMessage = notificationModel.recvMessage(message)
-  //   console.log('receiveMessage===>',message)
-    //
-  // }
+  dbConnectionPool = await getDbConnentionPool();
+  const conn: PoolClient = await db.getConnection();
+  const notificationModel = new Notification({},conn)
+
+  for (const record of event.Records) {
+    const message = JSON.parse(record.body)
+    const receiveMessage = notificationModel.recvMessage(message)
+  }
 }
