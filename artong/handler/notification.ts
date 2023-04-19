355cebcec15f37afcf62680c24ba07e1231e77d0
k3srpn <k3srpn@gmail.com>
2023-04-19 23:04:41 +0900

    feat: like noti handler

--- artong/handler/notification.ts
+++ artong/handler/notification.ts
@@ -2,16 +2,19 @@ import { Pool, PoolClient } from 'pg';
 import * as db from '../utils/db/db';
 import { Notification } from '../models';
 import { getDbConnentionPool } from '../init';
+import { SQSEvent } from 'aws-lambda';
 
-let dbConnectionPool: Pool;
+export let notiPool: Pool;
 
-export async function handler(event: any) {
-  dbConnectionPool = await getDbConnentionPool();
-  const conn: PoolClient = await db.getConnection();
+export async function handler(event: SQSEvent) {
+  notiPool = await getDbConnentionPool();
+  const conn: PoolClient = await db.getNotiConnection();
   const notificationModel = new Notification({},conn)
 
   for (const record of event.Records) {
     const message = JSON.parse(record.body)
-    const receiveMessage = notificationModel.recvMessage(message)
+    if (message.type === 'LIKE') {
+      notificationModel.recvLike(message)
+    }
   }
 }
