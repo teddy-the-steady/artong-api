42b5b408562764f1c620cc8a2f64c5450bcd288f
teddy <teddy.jeon@speedwagon.io>
2023-04-19 11:38:30 +0900

    added token_count/tokens_tobe_approved_count to queryProject

--- artong/models/projects/selectProject.sql
+++ artong/models/projects/selectProject.sql
@@ -5,7 +5,17 @@ SELECT
     EXISTS(SELECT s.member_id FROM subscribe s WHERE s.project_address = p.address AND s.member_id = ${member_id}) AS is_subscriber,
     EXISTS(SELECT c.member_id FROM contents c WHERE c.project_address = p.address AND c.member_id = ${member_id}) AS is_contributor,
   {{/exists}}
-  (SELECT MAX(token_id) FROM contents c WHERE c.project_address = p.address) AS max_token_id
+  (SELECT MAX(token_id) FROM contents c WHERE c.project_address = p.address) AS max_token_id,
+  (SELECT COUNT(*) FROM contents c
+    WHERE c.project_address = p.address
+    AND (c.token_id > 0 OR (c.is_redeemed IS NOT NULL AND c.status = 'APPROVED'))
+  ) AS token_count,
+  (SELECT COUNT(*) FROM contents c
+    WHERE c.project_address = p.address
+    AND c.is_redeemed = FALSE
+    AND (c.status != 'APPROVED' OR c.status IS NULL)
+    AND c.token_id IS NULL
+  ) AS tokens_tobe_approved_count
 FROM
   projects p
 WHERE
