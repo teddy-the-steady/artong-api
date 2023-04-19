6fdcd62a92efcaaa189cb9f89fb0d82dd3d2ccb6
k3srpn <k3srpn@gmail.com>
2023-04-19 23:04:54 +0900

    feat: insert noti sql

--- artong/models/notification/insertNotification.sql
+++ artong/models/notification/insertNotification.sql
@@ -1,5 +1,17 @@
-INSERT INTO
-  notification ( sender_id, receiver_id, content, category, redirect_on_click, content_id)
-VALUES
-  ( ${sender_id}, ${receiver_id}, ${content}, ${category}, ${redirect_on_click}, ${contend_id}) 
+INSERT INTO notification ( 
+    sender_id, 
+    receiver_id, 
+    noti_message, 
+    noti_type, 
+    {{#exists redirect_on_click}} redirect_on_click, {{/exists}}
+    content_id
+  )
+VALUES ( 
+    ${sender_id}, 
+    ${receiver_id}, 
+    ${noti_message}, 
+    ${noti_type}, 
+    {{#exists redirect_on_click}} ${redirect_on_click}, {{/exists}}
+    ${content_id} 
+  ) 
 RETURNING *
