f184015fba7244df6927a35af05a5fc9b3144fdd
k3srpn <k3srpn@gmail.com>
2023-04-24 22:01:40 +0900

    feat: follow noti type

--- artong/models/notification/notification.type.ts
+++ artong/models/notification/notification.type.ts
@@ -1,7 +1,9 @@
 const NotificationType = { 
   LIKE: 'LIKE', 
   CONTRIBUTE: 'CONTRIBUTE', 
-  CONTRIBUTE_APPROVE: 'CONTRIBUTE_APPROVE'
+  CONTRIBUTE_APPROVE: 'CONTRIBUTE_APPROVE',
+  FOLLOW_MEMBER: 'FOLLOW_MEMBER',
+  FOLLOW_PROJECT: 'FOLLOW_PROJECT',
 }
 type NotificationType = keyof typeof NotificationType
 
