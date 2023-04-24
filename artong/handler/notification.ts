d8d5b6bc9422eb1055f04b8f943c1e94f329480a
k3srpn <k3srpn@gmail.com>
2023-04-24 21:11:46 +0900

    feat: try catch 적용 & 파라미터 타입 추가

--- artong/handler/notification.ts
+++ artong/handler/notification.ts
@@ -4,18 +4,20 @@ import { Notification } from '../models';
 import { getDbConnentionPool } from '../init';
 import { SQSEvent } from 'aws-lambda';
 import { MessageBody } from '../models/notification/Notification';
+import { errorResponse } from '../utils/http/response';
 
 export let notiPool: Pool;
 
-export async function handler(event: SQSEvent) {
+export async function handler(event: SQSEvent, context: AWSLambda.Context, callback: AWSLambda.Callback) {
   notiPool = await getDbConnentionPool();
   const conn: PoolClient = await db.getNotiConnection();
   const notificationModel = new Notification({},conn)
-  
-  for (const record of event.Records) {
-    const message: MessageBody = JSON.parse(record.body)
-    if (message.noti_type === 'LIKE') {
+  try {
+    for (const record of event.Records) {
+      const message: MessageBody = JSON.parse(record.body)
       notificationModel.receiveMessage(message)
-    } 
+    }
+  } catch(error) {
+    errorResponse(event, error, callback)
   }
 }
