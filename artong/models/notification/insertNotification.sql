1e5f54cc90cc2674b13b92b827cd175a213ac08d
k3srpn <k3srpn@gmail.com>
2023-04-27 00:51:09 +0900

    fix: add comma if null data

--- artong/models/notification/insertNotification.sql
+++ artong/models/notification/insertNotification.sql
@@ -2,16 +2,16 @@ INSERT INTO notification (
     sender_id, 
     receiver_id, 
     noti_message, 
-    noti_type, 
-    {{#exists redirect_on_click}} redirect_on_click, {{/exists}}
-    content_id
+    noti_type 
+    {{#exists redirect_on_click}} ,redirect_on_click {{/exists}}
+    {{#exists content_id}} ,content_id {{/exists}}
   )
 VALUES ( 
     ${sender_id}, 
     ${receiver_id}, 
     ${noti_message}, 
-    ${noti_type}, 
-    {{#exists redirect_on_click}} ${redirect_on_click}, {{/exists}}
-    ${content_id} 
+    ${noti_type} 
+    {{#exists redirect_on_click}} ,${redirect_on_click} {{/exists}}
+    {{#exists content_id}} ,${content_id}  {{/exists}}
   ) 
 RETURNING *
