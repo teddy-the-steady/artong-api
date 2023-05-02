e12bd00e6527f6cb4590808ea79f82137da51a35
k3srpn <k3srpn@gmail.com>
2023-05-03 01:46:10 +0900

    build retry

--- artong/handler/socket.ts
+++ artong/handler/socket.ts
@@ -4,6 +4,7 @@ import {APIGatewayProxyWebsocketEventV2} from 'aws-lambda'
 const connect = async (event: APIGatewayProxyWebsocketEventV2) => {
   const { requestContext:{ domainName, stage, connectionId, connectedAt,routeKey } } = event
   try {
+    // TODO] endpoint 확인
     const client = new ApiGatewayManagementApiClient({
       region: 'ap-northeast-2',
       endpoint: 'http://localhost:3001'
