0d941a452a8ae48943b35763bfc933f6f2ba2b1c
k3srpn <k3srpn@gmail.com>
2023-05-03 01:10:14 +0900

    feat: Websocket connect handler

--- artong/handler/socket.ts
+++ artong/handler/socket.ts
@@ -1,9 +1,29 @@
+import { ApiGatewayManagementApiClient, PostToConnectionCommand } from '@aws-sdk/client-apigatewaymanagementapi'
+import {APIGatewayProxyWebsocketEventV2} from 'aws-lambda'
 
-const connect = async (event: any) => {
-  console.log('connect event', event)
+const connect = async (event: APIGatewayProxyWebsocketEventV2) => {
+  const { requestContext:{ domainName, stage, connectionId, connectedAt,routeKey } } = event
+  try {
+    const client = new ApiGatewayManagementApiClient({
+      region: 'ap-northeast-2',
+      endpoint: 'http://localhost:3001'
+    })
+
+    const encoder = new TextEncoder()
+
+    const postCommand = new PostToConnectionCommand({
+      ConnectionId: connectionId,
+      Data : encoder.encode(`Hello from ${routeKey}. You connected at ${connectedAt}`)
+    })
+
+    await client.send(postCommand)
+  } catch (error) {
+    return {statusCode: 5011}
+  }
+  return {statusCode: 200}
 }
 
-const disconenct = async (event: any) => {
+const disconenct = async (event:APIGatewayProxyWebsocketEventV2) => {
   console.log('disconnect event')
 }
 export {
