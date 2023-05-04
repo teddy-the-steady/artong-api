ea874f0fb2df8cb45f9f80406a8d206ddd820e13
k3srpn <k3srpn@gmail.com>
2023-05-04 21:45:32 +0900

    refactor: Modified handler's structure

--- artong/handler/socket.ts
+++ artong/handler/socket.ts
@@ -1,30 +1,19 @@
-import { ApiGatewayManagementApiClient, PostToConnectionCommand } from '@aws-sdk/client-apigatewaymanagementapi'
-import {APIGatewayProxyWebsocketEventV2} from 'aws-lambda'
+import { APIGatewayProxyWebsocketEventV2 } from 'aws-lambda'
 
-const connect = async (event: APIGatewayProxyWebsocketEventV2) => {
-  const { requestContext:{ domainName, stage, connectionId, connectedAt,routeKey } } = event
-  try {
-    const endpoint = process.env.IS_OFFLINE ? `http://localhost:3001` : `https://${domainName}/${stage}`
+const connectionManager = async (event: APIGatewayProxyWebsocketEventV2, context:AWSLambda.Context) => {
+  const { requestContext:{ eventType} } = event
 
-    const client = new ApiGatewayManagementApiClient({ region: 'ap-northeast-2', endpoint })
-    const encoder = new TextEncoder()
-
-    const postCommand = new PostToConnectionCommand({
-      ConnectionId: connectionId,
-      Data : encoder.encode(`Hello from ${routeKey}. You connected at ${connectedAt}`)
-    })
-
-    await client.send(postCommand)
-  } catch (error) {
-    return {statusCode: 500}
+  if(eventType === 'CONNECT') {
+    connect(event)
+  } else if(eventType ==='DISCONNECT') {
+    disconnect(event)
   }
-  return {statusCode: 200}
 }
 
-const disconenct = async (event:APIGatewayProxyWebsocketEventV2) => {
-  console.log('disconnect event')
-}
+const connect = async (event: APIGatewayProxyWebsocketEventV2 ) => {}
+
+const disconnect = async (event: APIGatewayProxyWebsocketEventV2) => {}
+
 export {
-  connect,
-  disconenct
+  connectionManager
 }
