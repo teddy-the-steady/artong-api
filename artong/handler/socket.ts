036f67f999aaea9d655e568a358ea23b6c6cb9b0
k3srpn <k3srpn@gmail.com>
2023-05-10 00:11:56 +0900

    feat: Implement send notifications when connection init

--- artong/handler/socket.ts
+++ artong/handler/socket.ts
@@ -3,51 +3,63 @@ import * as db from '../utils/db/db';
 import { Pool, PoolClient } from 'pg';
 import { Notification } from '../models';
 import { getDbConnentionPool } from '../init';
+import { SocketBody } from '../models/notification/notification.type';
+import { InternalServerError } from '../utils/error/errors';
 export let socketPool: Pool;
 
 const connectionManager = async (event: APIGatewayProxyWebsocketEventV2, context:AWSLambda.Context) => {
   const { requestContext:{ eventType} } = event
   
   if(eventType === 'CONNECT') {
-    connect(event, context)
-    return {
-      statusCode:200,
-      body: 'connected'
-    }
+    return connect()
   } else if(eventType ==='DISCONNECT') {
-    return disconnect(event)
-    
+    return disconnect()
   }
 }
 
-const connect = async (event: APIGatewayProxyWebsocketEventV2, context: AWSLambda.Context) => {
-  socketPool= await getDbConnentionPool();
-  const conn: PoolClient = await db.getSocketConnection();
-  const notificationModel = new Notification({},conn)
-
-  try{
-    const result = await notificationModel.getNotificationList()
-    return
-  }catch(error){
-  }
-}
-
-const disconnect = async (event: APIGatewayProxyWebsocketEventV2) => {
+const connect = async () => {
   return {
     statusCode: 200,
-    body: 'disconnected'
+    body: 'Artong halo!'
+  }
+}
+
+const disconnect = async () => {
+  return {
+    statusCode: 200,
+    body: 'Artong adios!'
   }
 }
 
 /**
  * @description default handler for websocket
  */
-const defaultHandler = async (event:any) => {
-  return {
-    statusCode: 200,
-    body: 'default'
+const defaultHandler = async (event: APIGatewayProxyWebsocketEventV2) => {
+  socketPool= await getDbConnentionPool();
+  const conn: PoolClient = await db.getSocketConnection();
+  const notificationModel = new Notification({},conn)
+
+  const { body, requestContext:{domainName,stage,connectionId} } = event
+  const { connectorId }= JSON.parse(body ?? '') as SocketBody
+
+  const notifications = await notificationModel.selectNotifications(connectorId)
+  const endpoint = process.env.IS_OFFLINE? 'http://localhost:3001': `https://${domainName}/${stage}`
+
+  try {
+    await notificationModel.sendNotificationsToClient(endpoint,connectionId, notifications)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(notifications)
+    }
+  } catch (error) {
+    throw new InternalServerError(error, null)
+  } finally {
+    db.release(conn)
   }
+
 }
+
 export {
   connect,
   disconnect,
