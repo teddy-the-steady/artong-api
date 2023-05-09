71e267f729a0d7e8d8a16ed5fd7ff4a64e880512
k3srpn <k3srpn@gmail.com>
2023-05-09 23:13:59 +0900

    feat: Send message when socket open

--- test/socket-client.js
+++ test/socket-client.js
@@ -3,4 +3,10 @@ const WebSocket = require('ws')
 const socket = new WebSocket('ws://localhost:3001')
 
 socket.on("open", () => {
+  const identifierMessage = {
+    type: 'IDENTIFIER',
+    userId: 315
+  }
+
+  socket.send(JSON.stringify(identifierMessage))
 })
