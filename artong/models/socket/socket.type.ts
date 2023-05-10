9620598b05b6e67b9cb45debd55228e402723c40
k3srpn <k3srpn@gmail.com>
2023-05-10 23:33:18 +0900

    feat: Add create socket connection body type

--- artong/models/socket/socket.type.ts
+++ artong/models/socket/socket.type.ts
@@ -1,7 +1,13 @@
 type SocketBody = {
   connectorId: number;
 }
+type CreateSocketConnectionBody = {
+  connectorId: number; 
+  connectionId: string; 
+  created_at: Date;
+}
 
 export {
-  SocketBody
+  SocketBody,
+  CreateSocketConnectionBody
 }
