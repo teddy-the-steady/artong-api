2cda65c3a2e4829a11f50432fa5d18b6f14a6229
k3srpn <k3srpn@gmail.com>
2023-05-12 02:13:41 +0900

    feat: When receive sqs message then send message to socket client

--- artong/handler/notification.ts
+++ artong/handler/notification.ts
@@ -6,20 +6,28 @@ import { SQSEvent } from 'aws-lambda';
 import { InternalServerError } from '../utils/error/errors';
 import { } from '../models/notification/notification.type';
 import { NotificationQueueBody } from '../models/queue/queue.type';
+import { Socket } from '../models/socket/Socket';
 
 export let notiPool: Pool;
 
 export async function handler(event: SQSEvent, context: AWSLambda.Context, callback: AWSLambda.Callback) {
   console.log(event.Records)
   notiPool = await getDbConnentionPool();
-  const conn: PoolClient = await db.getNotiConnection();
-  const notificationModel = new Notification({},conn)
-
+  
   try {
+    const conn: PoolClient = await db.getNotiConnection();
+    const notificationModel = new Notification({},conn)
+    const socket = new Socket({}, conn)
+    
     for (const record of event.Records) {
       const message: NotificationQueueBody= JSON.parse(record.body)
+      const {receiver_id} = message
+
       await notificationModel.createNotification(message)
-      return {data: 'success'}
+      const connection = await socket.selectSocketConnection({connectorId: receiver_id})
+      const endpoint = socket.generateEndpoint(connection.domain_name, connection.stage)
+
+      connection.connection_id && await socket.sendMessageToClient(endpoint, connection.connection_id, { data: message})
     }
   } catch (error) {
     console.error('notification handler error', error)
