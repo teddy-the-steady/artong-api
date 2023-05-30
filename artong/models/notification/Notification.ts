ddc7d667a81f9020c6bdbc677e98f3c878a01314
k3srpn <k3srpn@gmail.com>
2023-05-31 01:36:57 +0900

    remove: Remove unneccessary property in notification model

--- artong/models/notification/Notification.ts
+++ artong/models/notification/Notification.ts
@@ -2,7 +2,6 @@ import { IsDate, IsEnum, IsInt, IsOptional, IsString } from "class-validator";
 import { PoolClient } from "pg";
 import * as db from "../../utils/db/db";
 import Models from "../Models";
-import { NotificationType} from "./notification.type";
 import { InternalServerError } from "../../utils/error/errors";
 import { NotificationQueueBody } from "../queue/queue.type";
 
@@ -11,32 +10,6 @@ const selectNotifications = require('./selectNotifications.sql')
 const updateNotifications = require('./updateNotifications.sql')
 
 class Notification extends Models {
-  @IsInt()
-  id!: number;
-  @IsEnum(NotificationType)
-  noti_type!: NotificationType;
-  @IsInt()
-  sender_id!: number;
-  @IsInt()
-  receiver_id!: number;
-  @IsDate()
-  @IsOptional()
-  read_at?: Date;
-  @IsString()
-  @IsOptional()
-  redirect_on_click?: string | null;
-  @IsString()
-  @IsOptional()
-  noti_message!: string;
-  @IsInt()
-  @IsOptional()
-  content_id?: number | null;
-  @IsDate()
-  created_at!: Date;
-  @IsDate()
-  @IsOptional()
-  updated_at?: Date | null;
-  
   constructor(data: Partial<Notification> = {}, conn: PoolClient) {
     super(conn);
     Object.assign(this, data);
