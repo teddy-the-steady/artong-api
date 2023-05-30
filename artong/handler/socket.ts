b8d768559d7dd866f2767c5bdc37ffb73fd46805
k3srpn <k3srpn@gmail.com>
2023-05-31 01:23:42 +0900

    refactor: Socket connection intialize during websocket handshake

--- artong/handler/socket.ts
+++ artong/handler/socket.ts
@@ -1,88 +1,65 @@
-import { APIGatewayProxyWebsocketEventV2 } from 'aws-lambda'
+import { APIGatewayProxyWebsocketEventV2, Callback } from 'aws-lambda'
 import * as db from '../utils/db/db';
 import { Pool, PoolClient } from 'pg';
-import { Notification } from '../models';
 import { getDbConnentionPool } from '../init';
 import { InternalServerError } from '../utils/error/errors';
-import { CreateSocketConnectionBody, SocketBody } from '../models/socket/socket.type';
 import { Socket } from '../models/socket/Socket';
+import requestInit from '../utils/http/request';
 export let socketPool: Pool;
 
 
-const connectionManager = async (event: APIGatewayProxyWebsocketEventV2, context:AWSLambda.Context) => {
-  const { requestContext:{ eventType} } = event
+const connectionManager = async (event: APIGatewayProxyWebsocketEventV2, context:any, callback:Callback) => {
+  const { requestContext:{ eventType } } = event
   
-  if(eventType === 'CONNECT') {
-    return connect(event)
-  } else if(eventType ==='DISCONNECT') {
-    return disconnect(event)
+  if (eventType === 'CONNECT') {
+    // Must set callback before initConnection process
+    callback(null, { statusCode: 200 })
+
+    await initConnection(event).catch((error)=>callback(error, null))
+  } else if (eventType === 'DISCONNECT') {
+    await disposalConnection(event).catch((error)=> callback(error,null))
+
+    callback(null, { statusCode: 200 }) 
   }
 }
 
-const connect = async (event: APIGatewayProxyWebsocketEventV2) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({message: 'Artong hola!'})
+const initConnection = async (event: APIGatewayProxyWebsocketEventV2) => {
+  socketPool= await getDbConnentionPool();
+  const conn: PoolClient = await db.getSocketConnection();
+  const { requestContext:{ domainName, stage, connectionId } } = event
+
+  try {
+    const req = await requestInit(event, conn)
+    const connectorId = req.member.id
+    
+    const socket = new Socket({}, conn)
+    await socket.createSocketConnection({ connectionId, connectorId, domainName, stage })
+  } catch (error) {
+    throw new InternalServerError(error, null)
+  } finally {
+    db.release(conn)
   }
 }
 
-const disconnect = async (event: APIGatewayProxyWebsocketEventV2) => {
+const disposalConnection = async (event: APIGatewayProxyWebsocketEventV2) => {
   socketPool= await getDbConnentionPool();
   const conn: PoolClient = await db.getSocketConnection();
 
-  const { requestContext: { connectionId, } } = event
+  const { requestContext: { connectionId } } = event
   
   try {
     const socket = new Socket({}, conn)
 
     await socket.deleteSocketConnection(connectionId)
-
-    return { statusCode: 200 }
   } catch (error) {
     throw new InternalServerError(error, null)
   } finally {
     db.release(conn)
   }
 }
-
-/**
- * @description Init handler for websocket
- */
-const initHandler = async (event: APIGatewayProxyWebsocketEventV2) => {
-  socketPool= await getDbConnentionPool();
-  const conn: PoolClient = await db.getSocketConnection();
-  
-  const { body, requestContext:{ domainName, stage, connectionId } } = event
-  const { data: { connectorId }}= JSON.parse(body ?? '') as SocketBody
-  const notificationModel = new Notification({},conn)
-  const notifications = await notificationModel.selectNotifications(connectorId)
-
-  try {
-    const socket = new Socket({}, conn)
-    const apigatewaymanagementapi = socket.getApiGatewayManagementApi({domainName, stage})
-    const encoder = new TextEncoder()
-    const data: CreateSocketConnectionBody = { connectionId, connectorId, domainName, stage }
-
-    await socket.createSocketConnection(data)
-
-    await apigatewaymanagementapi.postToConnection({
-      ConnectionId: connectionId,
-      Data: encoder.encode(JSON.stringify({ data: notifications }))
-    })
-
-    return { statusCode: 200 }
-  } catch (error) {
-    throw new InternalServerError(error, null)
-  } finally {
-    db.release(conn)
-  }
-}
-
-
 
 export {
-  connect,
-  disconnect,
+  initConnection,
+  disposalConnection,
   connectionManager,
-  initHandler,
 }
