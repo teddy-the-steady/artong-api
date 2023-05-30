16d39fef97f0607116da8e45d2e064b7ac4303aa
k3srpn <k3srpn@gmail.com>
2023-05-31 01:39:36 +0900

    Revert "feat(reaction): add type validator in Reaction model"
    This reverts commit d84ae1365ca32ff620922353b09261b345ca5c6f.

--- artong/models/reactions/ContentReactions.ts
+++ artong/models/reactions/ContentReactions.ts
@@ -1,21 +1,15 @@
 import { PoolClient } from 'pg';
 import * as db from '../../utils/db/db';
 import Models from '../Models';
-import { IsDate, IsInt, IsOptional } from 'class-validator';
 const insertContentReactions = require('./insertContentReactions.sql')
 const selectTotalLikesByContent = require('./selectTotalLikesByContent.sql')
 
 class ContentReactions extends Models {
-	@IsInt()
-	reaction_id!: number;
-	@IsInt()
-	content_id!: number;
-	@IsInt()
-	member_id!: number;
-	@IsDate()
-	created_at!: Date;
-	@IsDate()
-	@IsOptional()
+	reaction_id?: number;
+	content_id?: number;
+	member_id?: number;
+
+	created_at?: Date;
 	updated_at?: Date;
 
 	constructor(data: Partial<ContentReactions> = {}, conn: PoolClient) {
@@ -24,9 +18,9 @@ class ContentReactions extends Models {
 	}
 
 	async createContentReaction(
-		reaction_code: string,
-		content_id: number,
-		member_id: number
+		reaction_code?: string,
+		content_id?: number,
+		member_id?: number
 	): Promise<ContentReactions> {
 		try {
 			const result = await db.execute(this.conn, insertContentReactions, {
@@ -41,7 +35,7 @@ class ContentReactions extends Models {
 	}
 
 	async getTotalLikesByContent(
-		content_id: number,
+		content_id?: number,
 	): Promise<{ total_likes: string }> {
 		try {
 			const result = await db.execute(this.conn, selectTotalLikesByContent, {
