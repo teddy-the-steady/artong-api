37350a3a82128d16e30ddc0f76a4ad40600971a1
k3srpn <k3srpn@gmail.com>
2023-05-31 01:24:35 +0900

    chore: Socket test code chores

--- test/socket-client.js
+++ test/socket-client.js
@@ -1,14 +1,10 @@
 // Excute command: node test/socket-client.js
 const WebSocket = require('ws')
-const socket = new WebSocket('wss://yyhdnvomg3.execute-api.ap-northeast-2.amazonaws.com/stage')
+// const socket = new WebSocket('wss://yyhdnvomg3.execute-api.ap-northeast-2.amazonaws.com/stage')
+const socket = new WebSocket('ws://127.0.0.1:3001?member_id=315')
 
 socket.on("open", () => {
-  const identifierMessage = {
-    action: 'init',
-    data: { connectorId: 315 }
-  }
-
-  socket.send(JSON.stringify(identifierMessage))
+  console.log("open")
 })
 
 socket.on("message", (data) => {
