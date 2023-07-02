f15f5ffd84464b64a944487f890bba12fb9d612e
k3srpn <k3srpn@gmail.com>
2023-07-02 19:33:40 +0900

    refactor: Notification is return notification that binding sender, receiver, content

--- artong/models/notification/Notification.ts
+++ artong/models/notification/Notification.ts
@@ -1,15 +1,27 @@
-import { IsDate, IsEnum, IsInt, IsOptional, IsString } from "class-validator";
 import { PoolClient } from "pg";
 import * as db from "../../utils/db/db";
 import Models from "../Models";
 import { InternalServerError } from "../../utils/error/errors";
 import { NotificationQueueBody } from "../queue/queue.type";
+import { NotificationTopic, NotificationWrapper } from "./notification.type";
 
 const insertNotification = require('./insertNotification.sql')
 const selectNotifications = require('./selectNotifications.sql')
+const selectNotificationByPrimaryKey = require('./selectNotificationByPrimaryKey.sql')
 const updateNotifications = require('./updateNotifications.sql')
 
+
 class Notification extends Models {
+  id?: number;
+  receiver_id!: number;
+  sender_id!: number;
+  topic!: NotificationTopic;
+  content_id?: number;
+  redirect_on_click?: string;
+  read_at?: Date;
+  created_at!: Date;
+  updated_at?: Date;
+
   constructor(data: Partial<Notification> = {}, conn: PoolClient) {
     super(conn);
     Object.assign(this, data);
@@ -26,16 +38,30 @@ class Notification extends Models {
     } 
   }
 
-  async selectNotifications(connectorId: number):Promise<Notification[]> {
-    try{
-      return await db.execute(this.conn, selectNotifications,{connectorId})
-    } catch(error){
+  async selectNotificationsByMemberPK(memberId: number, skip?:number, take?: number):Promise<Notification[]> {
+    try {
+      const result = await db.execute(this.conn, selectNotifications,{ 
+        memberId,
+        skip,
+        take
+      })
+      return result
+    } catch(error) {
       throw new InternalServerError(error, null)
     } 
   }
 
+  async selectNotificationByPrimaryKey(id: number): Promise<any>{
+    try {
+      const result = await db.execute(this.conn, selectNotificationByPrimaryKey, { id })
+      return result[0]
+    } catch(error) {
+      throw new InternalServerError(error, null)
+    }
+  }
+
   async readNotifications(notificationIds: number[]) {
-    try{
+    try {
       return await db.execute(this.conn, updateNotifications, { 
         readAt: new Date(),
         updatedAt: new Date(), 
@@ -43,6 +69,36 @@ class Notification extends Models {
       })
     } catch(error) {
       throw new InternalServerError(error,null)
+    }
+  }
+
+  async notificationWrapper(id: number): Promise<NotificationWrapper | void> {
+    const notification = await this.selectNotificationByPrimaryKey(id)
+
+    return {
+      id,
+      topic: notification.topic,
+      redirect_on_click: notification.redirect_on_click,
+      read_at: notification.read_at,
+      created_at: notification.created_at,
+      from: {
+        member: {
+          id: notification.sender_id,
+          username: notification.sender_username,
+          profile_thumbnail_s3key: notification.sender_profile_thumbnail_s3key,
+        }
+      },
+      to: {
+        member: {
+          id: notification.receiver_id,
+          username: notification.receiver_username,
+          profile_thumbnail_s3key: notification.receiver_profile_thumbnail_s3key
+        }
+      },
+      content: notification.content_id ? {
+        id: notification.content_id,
+        content_thumbnail_s3key: notification.content_thumbnail_s3key,
+      } : null
     }
   }
 }
---
+++ artong/models/notification/selectNotificationByPrimaryKey.sql
@@ -0,0 +1,20 @@
+SELECT
+  n.id as notification_id,
+  sender_id,
+  receiver_id,
+  topic,
+  redirect_on_click,
+  read_at,
+  n.created_at,
+  content_id,
+  c.content_thumbnail_s3key as content_thumbnail_s3key,
+  sender.username as sender_username,
+  sender.profile_thumbnail_s3key as sender_profile_thumbnail_s3key,
+  receiver.username as receiver_username,
+  receiver.profile_thumbnail_s3key as receiver_profile_thumbnail_s3key
+FROM notification n 
+JOIN member sender ON n.sender_id = sender.id
+JOIN member receiver ON n.receiver_id = receiver.id
+LEFT JOIN contents c ON n.content_id = c.id
+WHERE 1=1
+  AND n.id = ${id}
