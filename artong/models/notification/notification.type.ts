1e102ccb3db1c55ea476a08caa1c8e07c571363c
k3srpn <k3srpn@gmail.com>
2023-07-02 19:33:40 +0900

    refactor: Notification is return notification that binding sender, receiver, content

--- artong/models/notification/notification.type.ts
+++ artong/models/notification/notification.type.ts
@@ -1,13 +1,41 @@
-const NotificationCategory = { 
+const NotificationTopic = { 
   LIKE: 'LIKE', 
   CONTRIBUTE: 'CONTRIBUTE', 
   CONTRIBUTE_APPROVE: 'CONTRIBUTE_APPROVE',
   FOLLOW_MEMBER: 'FOLLOW_MEMBER',
   FOLLOW_PROJECT: 'FOLLOW_PROJECT',
 }
-type NotificationCategory = keyof typeof NotificationCategory
+type NotificationTopic = keyof typeof NotificationTopic
+type NotificationSender = {
+  id: number;
+  username: string;
+  profile_thumbnail_s3key: string;
+}
+type NotificationReceiver = {
+  id: number;
+  username: string;
+  profile_thumbnail_s3key: string;
+}
 
+type NotificationWrapper = {
+  id: number;
+  topic: NotificationTopic;
+  redirect_on_click?: string;
+  read_at?: Date;
+  created_at: Date;
+  from : {
+    member: NotificationSender;
+  }
+  to: {
+    member: NotificationReceiver; 
+  }
+  content: {
+    id: number;
+    content_thumbnail_s3key: string;
+  } | null;
+}
 
 export {
-  NotificationCategory 
+  NotificationTopic,
+  NotificationWrapper
 }
--- artong/models/notification/selectNotifications.sql
+++ artong/models/notification/selectNotifications.sql
@@ -1,4 +1,7 @@
 SELECT * 
 FROM notification
 WHERE 1=1
-  AND receiver_id = ${connectorId}
+  AND receiver_id = ${memberId}
+ORDER BY created_at DESC
+{{#exists skip}} LIMIT ${take} {{/exists}} 
+{{#exists take}} OFFSET ${skip} {{/exists}}
