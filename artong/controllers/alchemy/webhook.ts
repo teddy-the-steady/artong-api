632ed8f4278fcd8acf3e9a7079c030a689356901
k3srpn <k3srpn@gmail.com>
2023-07-04 00:00:55 +0900

    Print log about log

--- artong/controllers/alchemy/webhook.ts
+++ artong/controllers/alchemy/webhook.ts
@@ -3,6 +3,11 @@ const receive = async function (body: any) {
   if(logs instanceof Array) {
     logs.map((log)=>{
       console.log("트랜잭션 발생!!",log.transaction)
+      log.transaction.logs.map((subLog: any)=>{
+        subLog.topics.map((topic: any)=>{
+          console.log("토픽", topic)
+        })
+      })
     })
   }
 }
