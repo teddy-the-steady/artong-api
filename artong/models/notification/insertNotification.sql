0f0cd231373a7265f3e8ae70607ff0866d00889d
k3srpn <k3srpn@gmail.com>
2023-07-06 13:39:21 +0900

    Fix query because field is modified

--- artong/models/notification/insertNotification.sql
+++ artong/models/notification/insertNotification.sql
@@ -1,16 +1,14 @@
 INSERT INTO notification ( 
     sender_id, 
     receiver_id, 
-    noti_message, 
-    noti_type 
+    topic
     {{#exists redirect_on_click}} ,redirect_on_click {{/exists}}
     {{#exists content_id}} ,content_id {{/exists}}
   )
 VALUES ( 
     ${sender_id}, 
     ${receiver_id}, 
-    ${noti_message}, 
-    ${noti_type} 
+    ${topic} 
     {{#exists redirect_on_click}} ,${redirect_on_click} {{/exists}}
     {{#exists content_id}} ,${content_id}  {{/exists}}
   ) 
