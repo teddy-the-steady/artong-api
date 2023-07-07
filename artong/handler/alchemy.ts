201a445ff54d7dc54edc334ea09a27efc5f2b9de
k3srpn <k3srpn@gmail.com>
2023-07-07 22:24:49 +0900

    Add log

--- artong/handler/alchemy.ts
+++ artong/handler/alchemy.ts
@@ -5,13 +5,11 @@ export function handler(event: APIGatewayProxyEvent): APIGatewayProxyResult {
   const logs = body.event.data.block.logs
 
   try{
-    console.log("알케미 웹훅", body)
-    console.log("알케미 이벤트", body.event)
-    console.log("알케미 이벤트데이터", body.event.data)
-    console.log("알케미 이벤트데이터블록", body.event.data.block)
-    console.log("알케미 이벤트데이터블록로그", body.event.data.block.logs)
-    console.log("알케미 from", body.event.data.block.logs.transaction.from)
-    console.log("알케미 to", body.event.data.block.logs.transaction.to)
+    if (body.event.data.block.logs) {
+      console.log("알케미 이벤트데이터블록로그", body.event.data.block.logs)
+      console.log("알케미 from", body.event.data.block.logs.transaction.from)
+      console.log("알케미 to", body.event.data.block.logs.transaction.to)
+    }
   } catch(e){
     console.log("알케미 에러",e)
     return {
