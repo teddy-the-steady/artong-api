b205a90c6e8ad4e4bf2bc4907a2b881d2ac0a456
k3srpn <k3srpn@gmail.com>
2023-07-07 22:22:54 +0900

    Add log

--- artong/handler/alchemy.ts
+++ artong/handler/alchemy.ts
@@ -4,23 +4,20 @@ export function handler(event: APIGatewayProxyEvent): APIGatewayProxyResult {
   const body = event.body && JSON.parse(event.body)
   const logs = body.event.data.block.logs
 
-  console.log("알케미 웹훅", body)
-  console.log("알케미 이벤트", body.event)
-  console.log("알케미 이벤트데이터", body.event.data)
-  console.log("알케미 이벤트데이터블록", body.event.data.block)
-  console.log("알케미 이벤트데이터블록로그", body.event.data.block.logs)
-  if(logs instanceof Array) {
-    logs.map((log)=>{
-      console.log("알케미 로그트랜잭션",log.transaction)
-
-      log.transaction.logs.map((subLog: any)=>{
-        console.log("알케미 서브로그", subLog)
-
-        subLog.topics.map((topic: any)=>{
-          console.log("알케미 토픽", topic)
-        })
-      })
-    })
+  try{
+    console.log("알케미 웹훅", body)
+    console.log("알케미 이벤트", body.event)
+    console.log("알케미 이벤트데이터", body.event.data)
+    console.log("알케미 이벤트데이터블록", body.event.data.block)
+    console.log("알케미 이벤트데이터블록로그", body.event.data.block.logs)
+    console.log("알케미 from", body.event.data.block.logs.transaction.from)
+    console.log("알케미 to", body.event.data.block.logs.transaction.to)
+  } catch(e){
+    console.log("알케미 에러",e)
+    return {
+      statusCode: 201,
+      body: 'OK',
+    }
   }
 
   return {
