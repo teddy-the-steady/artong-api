f6bee023943223c353ae34a44eb537252014a90a
k3srpn <k3srpn@gmail.com>
2023-07-07 22:59:21 +0900

    Add logs into log

--- artong/handler/alchemy.ts
+++ artong/handler/alchemy.ts
@@ -7,8 +7,11 @@ export function handler(event: APIGatewayProxyEvent): APIGatewayProxyResult {
   try{
     if (logs instanceof Array && logs.length > 0) {
       console.log("알케미 이벤트데이터블록로그", body.event.data.block.logs)
-      console.log("알케미 from", body.event.data.block.logs.transaction.from)
-      console.log("알케미 to", body.event.data.block.logs.transaction.to)
+      
+      logs.map((log)=>{
+        console.log("알케미 from", log.transaction.from )
+        console.log("알케미 to ", log.transaction.to)
+      })
     }
   } catch(e){
     console.log("알케미 에러",e)
