3108e7fa842e9e3ace7df26433ba6c30ea5085fc
teddy <teddy.jeon@speedwagon.io>
2024-01-08 15:05:17 +0900

    node v20, added @aws-sdk/client-ssm, updated ssmKeys.ts to new sdk package codes

--- artong/utils/common/ssmKeys.ts
+++ artong/utils/common/ssmKeys.ts
@@ -1,76 +1,82 @@
-const AWS = require('aws-sdk');
-const ssm = new AWS.SSM();
+import { SSMClient, GetParametersCommand, Parameter } from '@aws-sdk/client-ssm'
+const ssm = new SSMClient();
 
 const getDBKeys = async function(){
+  const getDBKeysCommand = new GetParametersCommand({
+    Names: [
+      `/db/${process.env.ENV}/host`,
+      `/db/${process.env.ENV}/database`,
+      `/db/${process.env.ENV}/user`,
+      `/db/${process.env.ENV}/password`,
+    ],
+    WithDecryption: true
+  });
+
   try {
-    const keys = await DBKeyPromise.promise();
+    const keys = await ssm.send(getDBKeysCommand);
     return formatKeys(keys.Parameters);
   } catch (error) {
     console.error(error);
   }
 }
-
-const DBKeyPromise = ssm.getParameters({
-  Names: [
-    `/db/${process.env.ENV}/host`,
-    `/db/${process.env.ENV}/database`,
-    `/db/${process.env.ENV}/user`,
-    `/db/${process.env.ENV}/password`,
-  ],
-  WithDecryption: true
-});
 
 const getApiKey = async function(){
+  const getApiKeysCommand = new GetParametersCommand({
+    Names: [
+      `/apikey/${process.env.ENV}/artong`,
+    ],
+    WithDecryption: true
+  });
+
   try {
-    const keys = await apiKeyPromise.promise();
+    const keys = await ssm.send(getApiKeysCommand);
     return formatKeys(keys.Parameters);
   } catch (error) {
     console.error(error);
   }
 }
-
-const apiKeyPromise = ssm.getParameters({
-  Names: [
-    `/apikey/${process.env.ENV}/artong`,
-  ],
-  WithDecryption: true
-});
 
 const getNftStorageKey = async function(){
+  const getStorageKeysCommand = new GetParametersCommand({
+    Names: [
+      `/nftStorage/${process.env.ENV}/apikey`,
+    ],
+    WithDecryption: true
+  });
+
   try {
-    const keys = await nftStorageKeyPromise.promise();
+    const keys = await ssm.send(getStorageKeysCommand);
     return formatKeys(keys.Parameters);
   } catch (error) {
     console.error(error);
   }
 }
-
-const nftStorageKeyPromise = ssm.getParameters({
-  Names: [
-    `/nftStorage/${process.env.ENV}/apikey`,
-  ],
-  WithDecryption: true
-});
 
 const getSmtpKeys = async function(){
+  const getSmtpKeysCommand = new GetParametersCommand({
+    Names: [
+      `/email/${process.env.ENV}/user`,
+      `/email/${process.env.ENV}/pass`,
+    ],
+    WithDecryption: true
+  });
+
   try {
-    const keys = await smtpKeyPromise.promise();
+    const keys = await ssm.send(getSmtpKeysCommand);
     return formatKeys(keys.Parameters);
   } catch (error) {
     console.error(error);
   }
 }
 
-const smtpKeyPromise = ssm.getParameters({
-  Names: [
-    `/email/${process.env.ENV}/user`,
-    `/email/${process.env.ENV}/pass`,
-  ],
-  WithDecryption: true
-});
+interface Ikeys {
+  [key: string]: string | undefined;
+}
+const formatKeys = function(keys: Parameter[] | undefined) {
+  if (!keys) return {}
 
-const formatKeys = function(keys: Array<any>) {
-  return keys.reduce((acc, cur) => {
+  return keys.reduce((acc: Ikeys, cur) => {
+    if (!cur.Name) return acc
     acc[cur.Name] = cur.Value;
     return acc;
   }, {});
